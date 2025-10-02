4c60f56ab528f7b316ee466b41c6d6d30d7f993b
lskld <theo.lillieskold@gmail.com>
2025-10-02 19:37:24 +0200

    Player sprites now chase player sprite

--- Assets/Code/EnemySprite.cs
+++ Assets/Code/EnemySprite.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 
 public class EnemySprite : MonoBehaviour
 {
+    [SerializeField] public float speed = 2f;
+    public GameObject enemyObject;
+    public GameObject playerObject;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerObject = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
+    {
+        Vector3 dirction = enemyObject.transform.position - playerObject.transform.position;
+        dirction = -dirction.normalized;
+        enemyObject.transform.position += dirction * Time.deltaTime * speed;
+    }
+
+    void FixedUpdate()
     {
         
     }
